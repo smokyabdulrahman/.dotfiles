8b176c08425aaa3ff34de01f29b3066d9f4cd313
Abdulrahman Alrahma <a.alrahama@gmail.com>
2025-05-26 23:21:01 +0100

    update nvim config + fix starship nested shell bug

--- zsh/.config/zsh/starship.zsh
+++ zsh/.config/zsh/starship.zsh
@@ -1,1 +1,6 @@
+if [[ "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select" || \
+      "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select-wrapped" ]]; then
+    zle -N zle-keymap-select "";
+fi
+
 eval "$(starship init zsh)"
