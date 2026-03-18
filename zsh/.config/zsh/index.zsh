240657a5a2586f6451aa6dbd26afc418a8f8bd6f
Abdulrahman Alrahma <a.alrahama@gmail.com>
2026-03-18 16:44:00 +0000

    revert

--- zsh/.config/zsh/index.zsh
+++ zsh/.config/zsh/index.zsh
@@ -1,8 +1,5 @@
 source $HOME/.config/secrets.zsh
-# 1password
-op inject --in-file "${HOME}/.config/zsh/env.zsh" | while read -r line; do
-  eval "$line"
-done
+source $HOME/.config/zsh/env.zsh
 source $HOME/.config/zsh/starship.zsh
 source $HOME/.config/zsh/aliases.zsh
 
